0054e48481d77555c8c67cc03f801d02042df4c2
Jared Blitzstein <mailing-list@blitzstein.net>
2016-05-21 21:09:14 -0400

    allows a set of lines to be passed in or provide a default list

--- app/services/line_router.rb
+++ app/services/line_router.rb
@@ -1,8 +1,8 @@
 class LineRouter
   attr_reader :lines
 
-  def initialize
-    @lines = [Line::HatLine, Line::ShoeLine]
+  def initialize(lines = [Line::HatLine, Line::ShoeLine])
+    @lines = lines
   end
 
   # returns the first line that a person is eligible to be in based on the eligibilty defined in the line itself
