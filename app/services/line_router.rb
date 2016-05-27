dbbd4389481966a6753a00d6e9dd55bfe9568b52
Jared Blitzstein <mailing-list@blitzstein.net>
2016-05-27 10:20:37 -0400

    adds error handling for case of having no lines (overriding default behavior)

--- app/services/line_router.rb
+++ app/services/line_router.rb
@@ -3,6 +3,7 @@ class LineRouter
 
   # the default could also be `Line.select(:type).distinct` so every line available would initialize
   def initialize(lines = [Line::HatLine, Line::ShoeLine, Line::DefaultLine])
+    raise ArgumentError, 'Need at least 1 line to handle routing' if (lines.nil? || lines.empty?)
     @lines = lines
   end
 
